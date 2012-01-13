ddb168433a3219d017fcfc8c1acbf2ce91fb0bd3
JenLHarris <jen_l_harris@yahoo.com>
2012-01-13 12:29:10 -0500

    Final

--- js/functions.js
+++ js/functions.js
@@ -6,9 +6,50 @@ Javascript multi-line comment
 alert('This is an alert.');
 
 //'prompt' is for user input
-var name = prompt("What's your name?");
+var name;
+while (!name){
+name = prompt("What's your name?");
+}
 
 //'console.log()' writes out the content of a variable, for debugging
 console.log(name);
 
-document.write('Hello, ' + name + '!');
+document.write('Hello, ' + name + '! ;)');
+
+isNameCool();
+
+for (var i = 0; i < 5; i++) {
+	document.write('<br>' + i);
+	document.write('<h1>' + i + '</h1>');
+}
+
+function isNameCool() {
+	var name = prompt("What's your name?");
+	if (name== 'Jen') {
+		document.write('<br>' + "That name is EVERYWHERE!");
+	}
+	else {
+		document.write('<br>' + "Your name is not so cool.");
+	}
+}
+
+var writeNameMultipleTimes = function () {
+	for (var i = 0; i < 5; i++) {
+		document.write('<br><strong>Jen</strong>');
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
