5d7ded79c2536de315e7cdd60e601e3da593800d
JenLHarris <jen_l_harris@yahoo.com>
2012-01-13 11:35:41 -0500

    Break time :)

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+// JavaScript single line comment
+/*
+Javascript multi-line comment
+*/
+//'alert' pops up a small wndow with an OK button.
+alert('This is an alert.');
+
+//'prompt' is for user input
+var name = prompt("What's your name?");
+
+//'console.log()' writes out the content of a variable, for debugging
+console.log(name);
+
+document.write('Hello, ' + name + '!');
